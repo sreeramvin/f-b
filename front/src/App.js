8e4791ab222d085554c6987860a9b96bc34a2379
sreeramvin <sreeramvin@gmail.com>
2020-08-24 19:25:48 +0530

    integrated with django

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import { Button } from 'reactstrap';
+
 import './App.css';
 
 function App() {
+  const [loading,setLoading]=useState(false);
+  const [items,setItems]=useState({});
+
+  const search = (evt) =>{
+    evt.preventDefault()
+    fetch(`http://127.0.0.1:8000/api/todo/`)
+    .then(res =>res.json())
+    .then(result=>{
+      setItems(result);
+      setLoading(true);
+      console.log(result);
+    }
+    );
+  }
+
+
   return (
+    <div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button color="danger" onClick={search}>Danger!</Button>
     </div>
+    {loading?(
+      <div>
+      {items.map(el => (
+        <li key={el.id}>{el.title}</li>
+      ))}
+      </div>
+
+    ):(<div>not available</div>)
+  }
+  </div>
   );
 }
 
